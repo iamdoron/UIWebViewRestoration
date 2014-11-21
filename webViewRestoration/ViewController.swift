0f340523c35dff61f122f08b11baeda51da644cc
iamdoron <doronpagot@gmail.com>
2014-11-21 19:51:37 +0200

    Initial Commit

---
+++ webViewRestoration/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  webViewRestoration
+//
+//  Created by Doron Pagot on 11/21/14.
+//  Copyright (c) 2014 Doron Pagot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
